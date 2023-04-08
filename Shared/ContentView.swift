c1ca53057b97bdca8041bc2ed926e01380b74f37
Catelijne W <24cwessels@pinewood.edu>
2023-04-08 14:03:09 -0700

    API WORKS!!!!!!

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,13 +12,15 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State var apiURL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=aa38365c9dbf479ebdb342c83d3c5141"
     
-    
-    @State var urlString = "https://newsapi.org/v2/everything?q=Apple&from=2023-03-08&sortBy=publishedAt&apiKey=aa38365c9dbf479ebdb342c83d3c5141"
     var body: some View {
+        
         ScrollView {
             LazyVStack {
                 ForEach(1...10, id: \.self) { count in
+                    
+                    
                     
                     Text("Hello, world")
                     Spacer()
@@ -27,7 +29,32 @@ struct ContentView: View {
                 }
             }
         }
+        .onAppear { self.loadData { (News)  in
+            //print(News)
+        } }
     }
+    
+    func loadData(completion: @escaping (News) ->  ()) {
+        
+        guard let url = URL(string: apiURL) else {
+            print("Invalid URL")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            let result = try! JSONDecoder().decode(News.self, from: data!)
+            
+            DispatchQueue.main.async {
+//                print(result)
+                print(result.articles[0].content)
+
+                completion(result)
+            }
+        }
+        .resume()
+    }
+        
+    
 
 }
 
@@ -35,6 +62,29 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct News : Codable {
+    var status : String?
+    var totalResults : Int?
+    var articles : [articlesData]
+}
+
+struct articlesData : Codable {
+    var source : sourceStruct
+    var author: String?
+    var title: String?
+    var description : String?
+    var url : String?
+    var urlToImage : String?
+    var publishedAt : String?
+    var content : String?
+    
+}
+
+struct sourceStruct : Codable {
+    var id: String?
+    var name : String?
 }
 
 
