6365e319ab20ce063da1c1e9aa963c4a6f2cbcb7
Catelijne W <24cwessels@pinewood.edu>
2023-04-08 10:26:51 -0700

    Initial Commit

---
+++ Shared/disabilities_enableApp.swift
@@ -0,0 +1,17 @@
+//
+//  disabilities_enableApp.swift
+//  Shared
+//
+//  Created by Cate Wessels on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct disabilities_enableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
