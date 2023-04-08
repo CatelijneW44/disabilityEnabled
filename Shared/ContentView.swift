81c3ac369f09cf41e5a4f0dde4148b87cc4c1bea
Catelijne W <24cwessels@pinewood.edu>
2023-04-08 10:59:24 -0700

    API KEY + lazy vert. stack + scroll view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world")
-            .padding()
+        ScrollView {
+            LazyVStack {
+                ForEach(1...10, id: \.self) { count in
+                    Text("Hello, world")
+                    Spacer()
+
+                    // API KEY aa38365c9dbf479ebdb342c83d3c5141
+                }
+            }
+        }
     }
 }
 
